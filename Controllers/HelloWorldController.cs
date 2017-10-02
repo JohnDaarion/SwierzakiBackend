62eff2b31375b954f4cf997a2166d6cf4b45c7ae
JohnDaarion <bomrocznyjestjan@gmail.com>
2017-10-02 18:51:14 +0200

    Add files via upload

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DatabaseConnectionProvider.Controllers
+{
+    [Route("api")]
+    public class HelloWorldController : Controller
+    {
+        [HttpGet]
+        [Route("HelloWorld")]
+        public string GetHelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
